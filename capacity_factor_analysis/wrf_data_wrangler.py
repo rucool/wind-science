05167ff098739716ed7ae076c72f2885d07109cd
Lori Garzio <lgarzio@marine.rutgers.edu>
2022-11-22 20:22:09 -0500

    Update wrf_data_wrangler.py
    - export data to .nc files

--- capacity_factor_analysis/wrf_data_wrangler.py
+++ capacity_factor_analysis/wrf_data_wrangler.py
@@ -2,8 +2,8 @@
 
 """
 Author: Lori Garzio on 11/10/2022
-Last modified: Lori Garzio 11/21/2022
-Grab RU-WRF u, v data from thredds for a user-defined time-range and height at specified locations
+Last modified: Lori Garzio 11/22/2022
+Grab RU-WRF U, V data from THREDDS for a user-defined time-range and height at specified locations and export as NetCDF
 """
 
 import os
@@ -11,7 +11,7 @@ import numpy as np
 import xarray as xr
 import pandas as pd
 import datetime as dt
-import pickle
+from collections import OrderedDict
 pd.set_option('display.width', 320, "display.max_columns", 15)  # for display in pycharm console
 
 
@@ -34,19 +34,7 @@ def haversine_dist(blon,blat,slon,slat):
     return distance
 
 
-def wind_uv_to_spd(u, v):
-    """
-    Calculates the wind speed from the u and v wind components
-    :param u: west/east direction (wind from the west is positive, from the east is negative)
-    :param v: south/noth direction (wind from the south is positive, from the north is negative)
-    :returns wspd: wind speed calculated from the u and v wind components
-    """
-    wspd = np.sqrt(np.square(u) + np.square(v))
-
-    return wspd
-
-
-def main(start_str, end_str, height, save_dir):
+def main(start_str, end_str, height, lease, state, save_dir):
     start_date = dt.datetime.strptime(start_str, '%Y%m%d')
     end_date = dt.datetime.strptime(end_str, '%Y%m%d') + dt.timedelta(hours=23)
 
@@ -58,49 +46,115 @@ def main(start_str, end_str, height, save_dir):
 
     location_csv = '/Users/garzio/Documents/repo/rucool/wind-science/capacity_factor_analysis/files/lease_centroids.csv'
     loc_df = pd.read_csv(location_csv)
-    for i, row in loc_df.iterrows():
-        if np.logical_or(row.state == 'New Jersey', row.state == 'NY/NJ'):
-            data = dict(time=np.array([], dtype='datetime64[ns]'),
-                        u=np.array([]),
-                        v=np.array([]))
+    loc_df['lease_code'] = loc_df['lease'].map(lambda x: x.split(' - ')[0].replace(' ', ''))
+    if lease:
+        df = loc_df[loc_df['lease_code'] == lease]
+    elif state:
+        df = loc_df[loc_df['state'] == state]
 
-            data['company'] = row.company
-            data['code'] = row.code
-            data['state'] = row.state
-            data['lease'] = row.lease
-            lease_code = row.lease.split(' - ')[0].replace(' ', '')
+    for midlon, midlat, co, st, ll, lease_code in zip(df['long'], df['lat'], df['company'], df['state'], df['lease'], df['lease_code']):
+        # find the closest WRF coordinate
+        d = haversine_dist(midlon, midlat, wrf_lon, wrf_lat)
+        dmin = np.argwhere(d.data == np.min(d.data))
+        di = dmin[0][0]
+        dj = dmin[0][1]
 
-            midlon = row['long']
-            midlat = row['lat']
+        ds_filter = ds.sel(south_north=di, west_east=dj)
+        ds_filter = ds_filter.sel(height=height)
 
-            # find the closest WRF coordinate
-            d = haversine_dist(midlon, midlat, wrf_lon, wrf_lat)
-            dmin = np.argwhere(d.data == np.min(d.data))
-            di = dmin[0][0]
-            dj = dmin[0][1]
+        wrf_xlat = ds_filter.XLAT.values
+        wrf_xlon = ds_filter.XLONG.values
 
-            ds_point = ds.sel(south_north=di, west_east=dj)
-            ds_point_height = ds_point.sel(height=height)
+        # create empty dictionary to which data are added
+        data = {
+            "coords": {
+                "time": {"dims": "time", "data": np.array([], dtype='datetime64[ns]')},
+                "height": {"dims": (), "data": np.array(height, dtype='int32')},
+                "XLAT": {"dims": (), "data": np.array(wrf_xlat, dtype='float32')},
+                "XLONG": {"dims": (), "data": np.array(wrf_xlon, dtype='float32')}
+            },
+            "attrs": {
+                "point_lat": midlat,
+                "point_lon": midlon,
+                "company": co,
+                "state": st,
+                "lease": ll,
+                "lease_code": lease_code
+            },
+            "dims": "time",
+            "data_vars": {
+                "U": {
+                    "dims": "time",
+                    "data": np.array([], dtype='float32'),
+                    "attrs": {
+                        "units": "m s-1",
+                        "long_name": "Eastward Wind Component",
+                        "standard_name": "eastward_wind",
+                        "description": "earth rotated u",
+                        "comment": f"subset from RU-WRF at XLAT {str(np.round(wrf_xlat, 6))}, "
+                                   f"XLONG {str(np.round(wrf_xlon, 6))}, height {height}m"
+                    }
+                },
+                "V": {
+                    "dims": "time",
+                    "data": np.array([], dtype='float32'),
+                    "attrs": {
+                        "units": "m s-1",
+                        "long_name": "Northward Wind Component",
+                        "standard_name": "northward_wind",
+                        "description": "earth rotated v",
+                        "comment": f"subset from RU-WRF at XLAT {str(np.round(wrf_xlat, 6))}, "
+                                   f"XLONG {str(np.round(wrf_xlon, 6))}, height {height}m"
+                    }
+                },
+            },
+        }
 
-            data['wrf_lat'] = ds_point_height.XLAT.values
-            data['wrf_lon'] = ds_point_height.XLONG.values
-            data['height'] = height
+        for tm in ds_filter.time.values:
+            print(tm)
+            data['coords']['time']['data'] = np.append(data['coords']['time']['data'], tm)
+            data['data_vars']['U']['data'] = np.append(data['data_vars']['U']['data'], ds_filter.sel(time=tm)['U'].values)
+            data['data_vars']['V']['data'] = np.append(data['data_vars']['V']['data'], ds_filter.sel(time=tm)['V'].values)
 
-            for tm in ds_point_height.time.values:
-                print(tm)
-                data['time'] = np.append(data['time'], tm)
-                data['u'] = np.append(data['u'], ds_point_height.sel(time=tm)['U'].values)
-                data['v'] = np.append(data['v'], ds_point_height.sel(time=tm)['V'].values)
+        outds = xr.Dataset.from_dict(data)
 
-            pickle_name = os.path.join(save_dir, f'{lease_code}-{start_str}_{end_str}.pickle')
-            with open(pickle_name, 'wb') as handle:
-                pickle.dump(data, handle, protocol=pickle.HIGHEST_PROTOCOL)
-            print(f'Finished downloading {row["code"]}\n')
+        # add created time to global attrs
+        datetime_format = '%Y%m%dT%H%M%SZ'
+        created = dt.datetime.utcnow().strftime(datetime_format)  # creation time Timestamp
+        time_start = pd.to_datetime(outds.time.values[0]).strftime(datetime_format)
+        time_end = pd.to_datetime(outds.time.values[-1]).strftime(datetime_format)
+
+        global_attributes = OrderedDict([
+            ('date_created', created),
+            ('time_coverage_start', time_start),
+            ('time_coverage_end', time_end),
+            ('comment', 'U and V subset from RU-WRF dataset at the specified location and height'),
+            ('data_source', mlink)
+        ])
+
+        global_attributes.update(outds.attrs)
+
+        outds = outds.assign_attrs(global_attributes)
+
+        # Add compression to all variables
+        encoding = {}
+        for k in outds.data_vars:
+            encoding[k] = {'zlib': True, 'complevel': 1}
+
+        encoding['time'] = dict(units='seconds since 1970-01-01 00:00:00', calendar='gregorian', zlib=False,
+                                _FillValue=False, dtype=np.double)
+
+        # save .nc file
+        save_file = os.path.join(save_dir, f'{lease_code}_{height}.nc')
+        outds.to_netcdf(save_file, encoding=encoding, format='netCDF4', engine='netcdf4', unlimited_dims='time')
+        print(f'Finished downloading {lease_code}\n')
 
 
 if __name__ == '__main__':
-    start = '20201201'
-    end = '20211130'
+    start = '20190101'
+    end = '20190101'
     height = 160
-    sDir = '/Users/garzio/Documents/rucool/bpu/wrf/capacity_factor_analysis'
-    main(start, end, height, sDir)
+    lease = None  # 'OCS-A0498'
+    state = 'New Jersey'  # 'New Jersey', 'NY/NJ'
+    sDir = '/Users/garzio/Documents/repo/rucool/wind-science/capacity_factor_analysis/files'
+    main(start, end, height, lease, state, sDir)
