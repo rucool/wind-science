2f1b4b3daaf554bef6d5aba65f4a6180dea5a579
Lori Garzio <lgarzio@marine.rutgers.edu>
2022-12-22 12:21:13 -0500

    add logging

--- functions/common.py
+++ functions/common.py
@@ -1,8 +1,24 @@
 #! /usr/bin/env python
 
 import os
+import logging
 import glob
 import numpy as np
 import pandas as pd
 import datetime as dt
 
+
+def setup_logger(name, loglevel, logfile):
+    logger = logging.getLogger(name)
+
+    # if the logger doesn't already exist, set it up
+    if not logger.handlers:
+        log_format = logging.Formatter('%(asctime)s%(module)s:%(levelname)s:%(message)s [line %(lineno)d]')
+        handler = logging.FileHandler(logfile)
+        handler.setFormatter(log_format)
+
+        log_level = getattr(logging, loglevel)
+        logger.setLevel(log_level)
+        logger.addHandler(handler)
+
+    return logger
