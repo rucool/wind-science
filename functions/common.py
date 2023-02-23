a95715e68afd2a52b459938fa44072d0106479f3
Lori Garzio <lgarzio@marine.rutgers.edu>
2023-02-23 10:03:58 -0500

    Update common.py
    - add functions to calculate wind speed and direction from u, v

--- functions/common.py
+++ functions/common.py
@@ -22,3 +22,29 @@ def setup_logger(name, loglevel, logfile):
         logger.addHandler(handler)
 
     return logger
+
+
+def wind_uv_to_dir(u, v):
+    """
+    Calculates the wind direction from the u and v component of wind.
+    Takes into account the wind direction coordinates is different than the
+    trig unit circle coordinate. If the wind direction is 360 then returns zero
+    (by %360)
+    Inputs:
+    u = west/east direction (wind from the west is positive, from the east is negative)
+    v = south/noth direction (wind from the south is positive, from the north is negative)
+    """
+    wdir = (270-np.rad2deg(np.arctan2(v, u))) % 360
+    return wdir
+
+
+def wind_uv_to_spd(u, v):
+    """
+    Calculates the wind speed from the u and v wind components
+    :param u: west/east direction (wind from the west is positive, from the east is negative)
+    :param v: south/noth direction (wind from the south is positive, from the north is negative)
+    :returns WSPD: wind speed calculated from the u and v wind components
+    """
+    wspd = np.sqrt(np.square(u) + np.square(v))
+
+    return wspd
