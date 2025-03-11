91147f1bc18b867c1c91136cc252fff98c5d957c
Lori Garzio <lgarzio@marine.rutgers.edu>
2025-03-11 15:05:43 -0400

    Create setup.py

---
+++ rmi-ocean-mixing/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='rmi-ocean-mixing',
+    version='1.0',
+    packages=find_packages(),
+    url='https://github.com/rucool/wind-science/rmi-ocean-mixing',
+    author='Lori Garzio',
+    author_email='lgarzio@marine.rutgers.edu',
+    description='Tools to analyze and plot wind energy data.'
+)
